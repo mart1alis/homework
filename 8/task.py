d546fa514ebc3d211e2267ef4e8cf82b257b234b
Marcel <khabirov06@vk.com>
2025-05-23 22:03:02 +0300

    Add files via upload

--- 8/task.py
+++ 8/task.py
@@ -1,1 +1,14 @@
-
+def cipher(text):
+    result = []
+    for char in text:
+        if 'a' <= char <= 'z':
+            result.append(chr(ord('z') - (ord(char) - ord('a'))))
+        elif 'A' <= char <= 'Z':
+            result.append(chr(ord('Z') - (ord(char) - ord('A'))))
+        else:
+            result.append(char)
+    return ''.join(result)
+print('Ввод:')
+text = input()
+print('Вывод:')
+print (cipher(text))
