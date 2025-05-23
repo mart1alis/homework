3e578bb7efe8aeaf1cbc33e28238c216b09c3869
Marcel <khabirov06@vk.com>
2025-05-24 00:01:14 +0300

    Update task.cpp

--- 5/task.cpp
+++ 5/task.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
 
+using namespace std;
+
+int main() {
+    // Чтение чисел из файлов A.txt и B.txt
+    ifstream fileA("A.txt");
+    ifstream fileB("B.txt");
+    
+    vector<int> numbersA, numbersB;
+    int num;
+    
+    while (fileA >> num) numbersA.push_back(num);
+    while (fileB >> num) numbersB.push_back(num);
+    
+    fileA.close();
+    fileB.close();
+  
+    return 0;
+}
