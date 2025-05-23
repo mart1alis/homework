c8167e9a9e1e18b29abcda433ba946eb63faee0c
Marcel <khabirov06@vk.com>
2025-05-23 23:53:55 +0300

    Add files via upload

--- 13/task.py
+++ 13/task.py
@@ -1,1 +1,17 @@
+from collections import Counter
 
+result = []
+
+a = list(map(int, open('A.txt').read().split()))
+b = list(map(int, open('B.txt').read().split()))
+
+S = set(a) & set(b)
+print(S)
+count_a = Counter(a)
+count_b = Counter(b)
+
+result = []
+for num in sorted(S):
+    result += [num] * max(count_a[num], count_b[num])
+
+open('C.txt', 'w').write(' '.join(map(str, result)))
