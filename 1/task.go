ab6b281f67f8bd8f8995ff2e508f3a55142ea52f
Marcel <khabirov06@vk.com>
2025-05-29 22:59:48 +0300

    Add files via upload

--- 1/task.go
+++ 1/task.go
@@ -1,1 +1,24 @@
+package main
 
+import "fmt"
+
+func bubble(arr []int) {
+    n := len(arr)
+    for i := 0; i < n-1; i++ {
+        for j := 0; j < n-i-1; j++ {
+            if arr[j] > arr[j+1] {
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            }
+        }
+    }
+}
+
+func main() {
+    arr := []int{1, 4, 2, 12, 9, 3, 6}
+    
+    fmt.Println("Исходный массив:", arr)
+    
+    bubble(arr)
+    
+    fmt.Println("Отсортированный массив:", arr)
+}
